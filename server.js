62742898bdd509e2e17168ada2036b2ba93c538c
Ashish Ranjan <ashish.ranjan@payu.in>
2021-05-16 16:25:51 +0530

    typo

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
 const next = require('next');
-const KoaRouter = require('koa-router');
 const dotenv = require("dotenv");
-const path = require('path');
-const bodyParser = require('koa-bodyparser');
-// const Mongo = require('koa-mongo');
 
 dotenv.config();
 
@@ -15,9 +12,12 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 
 const handle = app.getRequestHandler();
 
+const server = new Koa();
+const router = require('./server/rest/router')
+
+server.use(router.routes()).use(router.allowedMethods());
+
 app.prepare().then(() => {
-    const server = new Koa();
-    const router = new KoaRouter();
 
     const handleRequest = async (ctx) => {
         console.log(`Inside handleRequest`);
@@ -26,21 +26,10 @@ app.prepare().then(() => {
         ctx.res.statusCode = 200;
     };
 
-    // Routes
-    router.get('/test', ctx => {
-        ctx.body = `Hello, Test`
-    });
-    router.get('/test/:name', ctx => {
-        ctx.body = `Hello, ${ctx.params.name}`
-    });
-
     router.get("(/_next/static/.*)", handleRequest);
     router.get("/_next/webpack-hmr", handleRequest);
     // router.get("(.*)", verifyRequest(), handleRequest);
     router.get("(.*)", handleRequest);
-
-    server.use(router.allowedMethods());
-    server.use(router.routes());
 
     server.listen(port, () => {
         console.log(`server listening to port --> ${port}`)
---
+++ server/rest/router.js
@@ -0,0 +1,26 @@
+// This file is where you will create router for your application. Backend router are required within your app as the Admin API won’t accept client-side requests.
+
+const Router = require('@koa/router');
+const axios = require('axios');
+
+const router = new Router();
+
+// Test Route
+// router.get('/test/:name', ctx => {
+//     ctx.body = `Hello, ${ctx.params.name}`
+// });
+
+router.get('/test', ctx => {
+    ctx.body = `Hello, Test`
+});
+
+router.get('/test1', ctx => {
+    ctx.body = `Hello, Test 1`
+});
+
+router.get('/test2', ctx => {
+    ctx.body = `Hello, Test 2`
+});
+
+
+module.exports = router;
